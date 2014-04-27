7f041f8d48fb5c69dc269e592eddc8b13c53c23b
grdshch <alexander.korepov@gmail.com>
2014-04-27 18:52:03 +0400

    fixes and comments for the script, initial text in README

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 Getting-and-Cleaning-Data-Project
 =================================
 
-Getting and Cleaning Data Project
+### How to use the script
+1. Download the data archive from https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+2. Put the archive into directory with run_analysis.R script
+3. Set working directory in R to the directory with the data and the script
+4. Run the script - source('run_analysis.R')
+
+### Getting the tidy data sets
+The result of running the script is 2 data files:
+1. mean_std_data.txt - file with mean and std data with descriptive labels
+2. average_data.txt - file with average data for different subjects and activities
+Files are created in the same direcotry with the script and the data archive.
--- run_analysis.R
+++ run_analysis.R
@@ -1,14 +1,19 @@
+## Getting and Cleaning Data Project
+## Full description of the data:
+##   http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+## The data:
+##   https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
 
 
-## zip archive with data to process s
+## zip archive with data to process
 zipFile = 'getdata-projectfiles-UCI HAR Dataset.zip'
 if(!file.exists(zipFile)){
     stop(paste(zipFile, ' data archive can\'t be found'))
 }
-
-#unzip(zipFile)
+unzip(zipFile)
 setwd('UCI HAR Dataset')
 
+## loading train and test data
 x_train <- read.table('train/X_train.txt')
 x_test <- read.table('test/X_test.txt')
 y_train <- read.table('train/y_train.txt')
@@ -16,27 +21,33 @@ y_test <- read.table('test/y_test.txt')
 subject_train <- read.table('train/subject_train.txt')
 subject_test <- read.table('test/subject_test.txt')
 
+## merging train and test data
 x <- rbind(x_train, x_test)
 y <- rbind(y_train, y_test)
 subject <- rbind(subject_train, subject_test)
+names(subject) <- 'Subject'
 
+## loading feature names and selecting mean and std from them
 features <-read.table('features.txt', stringsAsFactors=FALSE)
 names(x) <- features[[2]]
 mean_std <- grep('-mean\\(\\)|-std\\(\\)', features[[2]])
 
-names(subject) <- 'Subject'
-
+## getting activity labels for the data set
 labels <- read.table('activity_labels.txt')
-activity <- labels[y[[1]], 2]
+activity <- data.frame(labels[y[[1]], 2])
 names(activity) <- 'Activity'
 
+## adding subject and activity labels to mean and std data set
 result <- cbind(subject, activity, x[, mean_std])
 
+## calculating average values for subjects and activities
 library(data.table)
 group <- c('Subject', 'Activity')
+dt <- data.table(result)
 tidy <- dt[, lapply(.SD, mean), by=group, .SDcols=3:68]
 
 setwd('..')
 
+## writing results
 write.table(result, 'mean_std_data.txt', quote=FALSE)
 write.table(tidy, 'average_data.txt', quote=FALSE)
